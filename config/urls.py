4ba9e7f071e0641b9b0d86c7a3f0045313235991
JoSePh-190 <141582744+JoSePh-190@users.noreply.github.com>
2026-06-21 17:45:22 +0100

    Add files via upload

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib.sitemaps.views import sitemap
 
 from core.sitemaps import (
@@ -17,4 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/models.py
+++ core/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to='projects/')
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
     github_url = models.URLField(blank=True)
     live_url = models.URLField(blank=True)
 
@@ -21,6 +21,9 @@ class Blog(models.Model):
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
